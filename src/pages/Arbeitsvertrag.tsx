0b9d36b787cfa3df3771cbc7d2dc9407485427c2
Naoufal Labrihmi <144787284+NaoufalLabrihmi@users.noreply.github.com>
2025-08-08 23:24:36 +0100

    ..

--- src/pages/Arbeitsvertrag.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ArbeitsvertragTable from '../components/ArbeitsvertragTable';
- 
-export default function ArbeitsvertragPage() {
-  return <ArbeitsvertragTable />;
-} 
